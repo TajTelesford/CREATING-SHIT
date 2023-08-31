7eaa7036426d7584fa60b3a9a27e639ffabc5feb
TajTelesford <TajTelesford@gmail.com>
2023-08-31 15:00:10 -0500

    Database connection Working - Change /w structire

---
+++ source/Utility/Assignment.java
@@ -0,0 +1,11 @@
+package Source.Utility;
+public class Assignment {
+    /*
+        Variables:
+        - Student Name
+        - Student ID (Primary Key - Database)
+        - Date Of Submittion
+        - Assignment informarion (Questions - Answers)
+        
+     */
+}
---
+++ source/Utility/Courses.java
@@ -0,0 +1,24 @@
+package Source.Utility;
+
+public class Courses {
+    
+    
+    private course[] CourseInformation;
+
+    public Courses(course[] courses)
+    {
+        CourseInformation = courses;
+    }
+
+    /*
+        - Display Courses and course assignments
+        - 
+     */
+    public void DisplayCourses()
+    {
+        for( int i = 0; i < CourseInformation.length; i++ )
+        {
+            System.out.println(CourseInformation[i]);
+        }
+    }
+}
